49501e9c3a41f178d1ebab442ceea9f1803e0a3a
filipetrinidad <fipe5766@gmail.com>
2021-04-08 18:55:21 -0300

    perceptron-xor

--- perceptron-xor.py
+++ perceptron-xor.py
@@ -1,17 +1,23 @@
 import numpy as np
 
-entradas = np.array([0,0], [0,1], [1,0], [1,1])
-saidas = np.array([0], [1], [1], [0])
+entradas = np.array([[0,0], [0,1], [1,0], [1,1]])
+saidas = np.array([[0], [1], [1], [0]])
 
-pesos0 = np.array([-0.345, -0.289, -0.134, 0.876, 0.387, -0.456])
-pesos1 = np.array([[0.765], [-0.234], [-0.492])
+pesos0 = np.array([[-0.345, -0.289, -0.134], [0.876, 0.387, -0.456]])
+pesos1 = np.array([[0.765], [-0.234], [-0.492]])
 
-treinos = 1000
+treinos = 100
 taxaAprendizado = 0.3
 momentum = 1
 
 def sigmoid(soma):
     return 1 / ( 1 + np.exp(-soma))
+
+def sigmoidDerivada(sig):
+    return sig * (1-sig)
+
+derivada0 = sigmoidDerivada(0.5)
+derivada1 = sigmoidDerivada(sigmoid)
 
 for i in range(treinos):
     camadaEntrada = entradas
@@ -20,4 +26,8 @@ for i in range(treinos):
 
     somaSinapse1 = np.dot(camadaOculta, pesos1)
     camadaSaida = sigmoid(somaSinapse1)
+
+    erroCamadaSaida = saidas - camadaSaida
+    media = np.mean(np.abs(erroCamadaSaida))
+    
 
